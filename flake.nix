64a105d66cfb97c8c1dd4f0be119404bb7a36dc1
Alex Ameen <alex.ameen.tx@gmail.com>
2022-10-22 17:57:27 -0500

    fix overlay export pattern

--- flake.nix
+++ flake.nix
@@ -4,8 +4,10 @@
 
   outputs = { nixpkgs, ak-nix, ... }: let
 
+    ytOverlays.deps    = ak-nix.ytOverlays.default;
     ytOverlays.laika   = import ./types/overlay.yt.nix;
-    ytOverlays.default = ytOverlays.laika;
+    ytOverlays.default = nixpkgs.lib.composeExtensions ytOverlays.deps
+                                                       ytOverlays.laika;
 
     libOverlays.deps  = ak-nix.libOverlays.default;
     libOverlays.laika = final: prev: {
@@ -14,6 +16,7 @@
     libOverlays.default = ak-nix.lib.composeExtensions libOverlays.deps
                                                        libOverlays.laika;
 
+    overlays.deps    = ak-nix.overlays.default;
     overlays.laika   = import ./overlay.nix;
     overlays.default = ak-nix.lib.composeExtensions ak-nix.overlays.default
                                                     overlays.laika;
