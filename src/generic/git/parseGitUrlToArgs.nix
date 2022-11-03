8eda5597ff21ddc48e824f3475177f9b86f1c851
Alex Ameen <alex.ameen.tx@gmail.com>
2022-11-03 11:52:44 -0500

    relax param type to allow non-encoded strings as values

--- src/generic/git/parseGitUrlToArgs.nix
+++ src/generic/git/parseGitUrlToArgs.nix
@@ -35,11 +35,12 @@
         s  = builtins.split "/${repo}\\.git" parsed.path;
         s0 = builtins.head s;
         d = if s0 == "" then dirOf parsed.path else s0;
-      in baseNameOf d;
+        b = baseNameOf d;
+      in if builtins.elem b ["" "."] then null else b;
       # FIXME: I think flake-ref do `.../foo.git/<REV>?<QUERY>`
       rev = if parsed.fragment != null then parsed.fragment else
         if parsed ? query.rev then parsed.query.rev else null;
-      #ref = if parsed ? query.ref then parsed.query.ref else null;
+      ref = if parsed ? query.ref then parsed.query.ref else null;
     };
     __processArgs = self: x:
       if builtins.isString x then { url = x; } else {
--- tests/generic.nix
+++ tests/generic.nix
@@ -14,6 +14,25 @@
         type = "git";
         url = "git+ssh://git@github.com/aakropotkin/rime.git#95aeaf83c247b8f5aa561684317ecd860476fcd6";
         user = "git";
+        ref  = null;
+      };
+    };
+
+    testParseGitUrl_1 = let
+      base = "https://code.tvl.fyi/depot.git";
+      ref  = "refs/heads/canon";
+      rev  = "57cf952ea98db70fcf50ec31e1c1057562b0a1df";
+    in {
+      expr     = lib.libfetch.parseGitUrl "${base}?rev=${rev}&ref=${ref}";
+      expected = {
+        host = "code.tvl.fyi";
+        owner = null;
+        repo = "depot";
+        inherit rev ref;
+        transport = "https";
+        type = "git";
+        url  = "${base}?rev=${rev}&ref=${ref}";
+        user = null;
       };
     };
 
