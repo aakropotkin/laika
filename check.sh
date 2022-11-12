42653095cb4f2f47725cce569c3a5d7800cddf0c
Alex Ameen <alex.ameen.tx@gmail.com>
2022-11-12 17:59:04 -0600

    upgrade check.sh

--- check.sh
+++ check.sh
@@ -7,15 +7,18 @@ set -eu;
 : "${SYSTEM:=$( $NIX eval --raw --impure --expr builtins.currentSystem; )}";
 : "${GREP:=grep}";
 
+export NIX_CONFIG='
+warn-dirty = false
+';
+
 nix_w() {
   { $NIX "$@" 3>&2 2>&1 1>&3|$GREP -v 'warning: unknown flake output'; }  \
     3>&2 2>&1 1>&3;
 }
 
-export NIX_CONFIG='
-warn-dirty = false
-';
-#nix_w flake check $NIX_FLAGS;
-#nix_w flake check $NIX_FLAGS --impure;
+trap '_es="$?"; exit "$_es";' HUP EXIT INT QUIT ABRT;
+
 nix_w flake check $NIX_FLAGS --system "$SYSTEM";
 nix_w flake check $NIX_FLAGS --system "$SYSTEM" --impure;
+
+$NIX eval .#lib --apply 'lib: builtins.deepSeq lib true';
